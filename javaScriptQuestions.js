1d16d5f3a9d9e4e5d206e9c0d2d30eef426b0be1
David Lara <david.a.lara05@gmail.com>
2023-01-20 19:42:27 -0600

    added first function

---
+++ javaScriptQuestions.js
@@ -0,0 +1,8 @@
+// This checks if the numbers are even or odd
+function evenOrOdd(number) {
+    if(number %2 === 0){
+        return("Even")
+    } else if (number %2 !== 0){
+        return("Odd")
+    }
+}
