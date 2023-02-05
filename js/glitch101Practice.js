ab02b243a1eaea3282be4d3db7eb158cb43b3e6b
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-02-04 20:36:36 -0600

    finish 1 - 8

---
+++ js/glitch101Practice.js
@@ -0,0 +1,46 @@
+//  Exercise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+// For more on variables, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables
+let onMarsRightNow = false
+
+
+// Exercise 2
+// For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+
+let fruits = ["mango", "banana", "guava", "kiwi", "strawberry"];
+
+
+// Exercise 3
+// Create a variable named vegetables and assign it an array of strings containing the following vegetables.
+// eggplant, broccoli, carrot, cauliflower, and zucchini
+
+var vegetables = ['eggplant', 'broccoli', 'carrot', 'cauliflower', 'zucchini'];
+
+// Exercise 4
+// Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+// *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
+fruits.push("tomato")
+
+
+// Exercise 6
+// add the string "tomato" onto the end of the vegetables array.
+// Recommend using the built-in JS operation to add to an array.
+vegetables.push("tomato");
+
+
+// Exercise 7
+// Given the array of numbers defined below, reverse the array of numbers that you created above.
+var someNumbers = numbers.reverse();
+
+
+// Exercise 8
+// Sort the vegetables in alphabetical order. Recommend finding a way to sort the array with a built-in method
+vegetables.sort();
