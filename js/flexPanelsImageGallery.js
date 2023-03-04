626dc08763274d350a86ac73b3f7d9232759b806
David Lara <david.a.lara05@gmail.com>
2023-03-03 22:24:21 -0600

    finish day 5

--- js/flexPanelsImageGallery.js
+++ js/flexPanelsImageGallery.js
@@ -5,4 +5,14 @@ function toggleOpen(){
     console.log("hrlo")
 }
 
+
+function  toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
