6f9b395b0647ededb440b5d03776ec82077fa4f3
David Lara <david.a.lara05@gmail.com>
2023-03-05 18:35:17 -0600

    created a array with data

--- js/typeAhead.js
+++ js/typeAhead.js
@@ -1,1 +1,12 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// this is a empty array till we push the data inside
+const cities = [];
+
+// fetching api
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    // data gives you the name of the cities
+    // we are using the spread operator to get all the data and not have a nested array and we are pushing the cities
+    .then(data => cities.push(...data))
