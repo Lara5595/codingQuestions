997f8c6796bbea1d46082473a361240593c6a753
David Lara <david.a.lara05@gmail.com>
2023-03-06 23:09:30 -0600

    Created file to practice

---
+++ js/searchCart.js
@@ -0,0 +1,21 @@
+// function attachAddToCart() {
+//
+//     // this is for the add to cart button
+//     const addItemsToCard = document.querySelectorAll(".add-to-cart-button");
+//
+//     // This is for the number of items
+//     const cartSize = document.getElementById("cart-size");
+//
+//     let numsItemsInCart = 0;
+//
+//     addItemsToCard.forEach(button => {
+//         button.addEventListener('click', function() {
+//             numsItemsInCart++
+//             cartSize.textContent = numsItemsInCart.toString();
+//
+//         });
+//     });
+//
+//
+//
+// }
