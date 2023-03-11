7cfe933d7e893f5a8f27236592aa4195e8971883
David Lara <david.a.lara05@gmail.com>
2023-03-10 23:36:12 -0600

    finish day 8

--- js/funWithHTML5Canvas.js
+++ js/funWithHTML5Canvas.js
@@ -7,11 +7,68 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-ctx.strokeStyle = '#BASA55';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 // this is set to false till we start drawing
 let isDrawing = false;
+
+// The line needs a start and an end
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
+
+
+function draw(e) {
+    if (!isDrawing) return; // stop the fn from running when they are not moused down
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if(direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+
+}
+
+// this check if the mouse is down then it will log
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+
+
+canvas.addEventListener('mousemove', draw)
+
+
+// this wont
+canvas.addEventListener('mouseup', function()  {
+    isDrawing = false
+})
+
+// this wont
+canvas.addEventListener('mouseout', function () {
+    isDrawing = false
+})
