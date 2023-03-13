f8a2118d79aba929fab865046394f647f47c8094
David Lara <david.a.lara05@gmail.com>
2023-03-12 23:41:57 -0500

    almost done with 11

---
+++ js/holdShiftToCheckMultipleCheckboxes.js
@@ -0,0 +1,31 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+function handleCheck(e){
+    // check if they had the shift key down
+    // and check that they are checking it
+    let inBetween = false;
+
+    if(e.shiftKey && this.checked) {
+        // go ahead and do what we please
+        // loop over every single checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox)
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween =  !inBetween;
+                console.log("starting to check in between")
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+
+        })
+    }
+
+
+    lastChecked = this
+}
+
+let lastChecked;
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
